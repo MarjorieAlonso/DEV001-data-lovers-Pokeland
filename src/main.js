dffcc84f0cec513f51a769f394a91da59084797e
RominaVidalVargas <roomii.2010.vidal@gmail.com>
2022-10-25 17:57:45 -0300

    extraer la data al 10

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+/* console.log(example, data); */
+const pokemon = data.pokemon;
+console.log("pokemon", pokemon);
+const div = document.getElementById("root");
+
+const drawCard = (pokemon) => {
+    return `<div>
+<li> # ${pokemon.num}</li>
+<li> Nombre: ${pokemon.name}</li>
+
+</div>`;
+}
+for (let i = 0; i < 10; i++) {
+    div.innerHTML += drawCard(pokemon[i]);
+}
