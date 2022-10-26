1fcf6f9a52d251a6f6a08ec6723189a7789e9dbe
RominaVidalVargas <roomii.2010.vidal@gmail.com>
2022-10-26 20:17:01 -0300

    data+css

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-/* console.log(example, data); */
+import data from './data/pokemon/pokemon.js';
+
+
+/* importar data y donde mostar*/
 const pokemon = data.pokemon;
 console.log("pokemon", pokemon);
 const div = document.getElementById("root");
 
 const drawCard = (pokemon) => {
     return `<div>
+<div id="a">
+<div id="b">
 <li> # ${pokemon.num}</li>
+<img src= "${pokemon.img}"/>
 <li> Nombre: ${pokemon.name}</li>
-
+</div></div>
 </div>`;
 }
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i <pokemon.length; i++) {
     div.innerHTML += drawCard(pokemon[i]);
 }
