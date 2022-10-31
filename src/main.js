c8b973392abb9bb3adf30c4c192cb3e26c187892
MarjorieAlonso <114000910+MarjorieAlonso@users.noreply.github.com>
2022-10-31 15:54:18 -0300

    cambio de color tarjetas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { example} from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
@@ -8,15 +7,69 @@ const pokemon = data.pokemon;
 const div = document.getElementById("root");
 /*cartas de cada pokemon*/
 const drawCard = (pokemon) => {
+    let clase = "";
+    if (pokemon.type.includes("grass")) {
+        clase = "verde"
+    } else if (pokemon.type.includes("normal")) {
+        clase = "grisClaro"
+    }
+    else if (pokemon.type.includes("water") ) {
+        clase = "azulClaro"
+    }
+    else if (pokemon.type.includes("fire")) {
+        clase = "naranja"
+    }
+    else if (pokemon.type.includes("bug")) {
+        clase = "verdeClaro"
+    }
+    else if (pokemon.type.includes("poison")) {
+        clase = "moradoClaro"
+    }
+    else if (pokemon.type.includes("electric")) {
+        clase = "amarillo"
+    }
+    else if (pokemon.type.includes("ground")) {
+        clase = "cafeOscuro"
+    }
+    else if (pokemon.type.includes("flying")) {
+        clase = "celesteClaro"
+    }
+   
+    else if (pokemon.type.includes ("psychic")){
+        clase = "rojo"
+    }
+       
+    else if (pokemon.type.includes("fairy")) {
+        clase = "rosado"
+    }
+       
+    else if (pokemon.type.includes("fighting")) {
+        clase = "fucsia"
+    }
+       
+    else if (pokemon.type.includes("rock") ){
+        clase = "cafeClaro"
+    }
+    else if (pokemon.type.includes("ghost")) {
+        clase = "moradoOscuro"
+    }
+    else if (pokemon.type.includes("dark")) {
+        clase = "grafito"
+    }
+
+
     return `<div>
-<div id="a"> 
-<div id="b">
+<div class="a ${clase}"> 
+<div class="b">
 <li> # ${pokemon.num}</li>
 <img src= "${pokemon.img}"/>
 <li> Nombre: ${pokemon.name}</li>
 </div></div>
 </div>`;
 }
-for (let i = 0; i <pokemon.length; i++) {
+for (let i = 0; i < pokemon.length; i++) {
     div.innerHTML += drawCard(pokemon[i]);
 }
+
+
+
