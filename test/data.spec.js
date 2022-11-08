26026b455a1e797def210129ed8b510fa462ba06
MarjorieAlonso <114000910+MarjorieAlonso@users.noreply.github.com>
2022-11-08 14:19:29 -0300

    test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,30 @@
-import { example, anotherExample } from '../src/data.js';
+import { searchFilter,ordenAzPokemon} from '../src/data.js';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe('searchFilter', () => {
+  it('filtra por nombre de pokemon', () => {
+    let data= [{name:"bulbasaur"},{name:"pikachu"}]
+    let nombres="bulbasaur";
+    let resultReal=searchFilter(data,nombres);
+    let resultExpect= [{name:"bulbasaur"}]
+    expect(resultReal).toEqual(resultExpect);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+
+describe("ordenAzPokemon", () => {
+  it("ordena los pokemon de la a a la z", () => {
+    let orden=[{name:"abra"},{name:"aerodactyl"}]
+    let ascendente= "abra";
+    let real= ordenAzPokemon(orden,ascendente);
+    let expect=[{name:"abra"}]
+
+    expect(real).toEqual(expect);
+  });
   });
 
-  it('returns `anotherExample`', () => {
+  /* it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
   });
-});
+}); */
