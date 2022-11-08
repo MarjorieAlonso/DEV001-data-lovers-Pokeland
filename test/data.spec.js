896bdcb8ae40455d9f3542803c42b449231a9ce8
MarjorieAlonso <114000910+MarjorieAlonso@users.noreply.github.com>
2022-11-08 17:10:02 -0300

    intento de segundo test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,12 @@
-import { searchFilter,ordenAzPokemon} from '../src/data.js';
+import { searchFilter, ordenAzPokemon } from '../src/data.js';
 
 
 describe('searchFilter', () => {
   it('filtra por nombre de pokemon', () => {
-    let data= [{name:"bulbasaur"},{name:"pikachu"}]
-    let nombres="bulbasaur";
-    let resultReal=searchFilter(data,nombres);
-    let resultExpect= [{name:"bulbasaur"}]
+    let data = [{ name: "bulbasaur" }, { name: "pikachu" }]
+    let nombres = "bulbasaur";
+    let resultReal = searchFilter(data, nombres);
+    let resultExpect = [{ name: "bulbasaur" }]
     expect(resultReal).toEqual(resultExpect);
   });
 });
@@ -14,17 +14,23 @@ describe('searchFilter', () => {
 
 
 describe("ordenAzPokemon", () => {
-  it("ordena los pokemon de la a a la z", () => {
-    let orden=[{name:"abra"},{name:"aerodactyl"}]
-    let ascendente= "abra";
-    let real= ordenAzPokemon(orden,ascendente);
-    let expect=[{name:"abra"}]
+  it("deberia ser una funcion", () => {
 
-    expect(real).toEqual(expect);
-  });
-  });
 
-  /* it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+    expect(typeof ordenAzPokemon).toBe("function");
   });
+});
+it('Que retorne el array ordenado de la A-Z', () => {
+  const arrAsc = [
+    { name: "bulbasaur" }, { name: "abra" }, { name: "charmander" }
+  ]
+  const resultado = [{ name: "abra" }, { name: "bulbasaur" }, { name: "charmander" }]
+  expect(ordenAzPokemon(arrAsc)).toEqual(resultado)
+});
+
+
+
+/* it('returns `anotherExample`', () => {
+  expect(anotherExample()).toBe('OMG');
+});
 }); */
