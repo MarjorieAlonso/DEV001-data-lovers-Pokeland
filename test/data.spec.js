3ff1d4f6ca5b89d6d4b9134a022018263a8f292e
RominaVidalVargas <roomii.2010.vidal@gmail.com>
2022-11-08 21:17:00 -0300

    filtros -Gen

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,6 @@
-import { searchFilter, ordenAzPokemon } from '../src/data.js';
+import { searchFilter, ordenAzPokemon, ordenZaPokemon, ordenAscendente, ordenDescendente, ordenKanto, ordenJohto } from '../src/data.js';
 
-
+/*test search*/
 describe('searchFilter', () => {
   it('filtra por nombre de pokemon', () => {
     let data = [{ name: "bulbasaur" }, { name: "pikachu" }]
@@ -12,7 +12,7 @@ describe('searchFilter', () => {
 });
 
 
-
+/*test de AZ*/
 describe("ordenAzPokemon", () => {
   it("deberia ser una funcion", () => {
 
@@ -29,8 +29,103 @@ it('Que retorne el array ordenado de la A-Z', () => {
 });
 
 
+/*test za*/
+describe("ordenZaPokemon", () => {
+  it("deberia ser una funcion", () => {
 
-/* it('returns `anotherExample`', () => {
-  expect(anotherExample()).toBe('OMG');
+
+    expect(typeof ordenZaPokemon).toBe("function");
+  });
 });
-}); */
+it('Que retorne el array ordenado de la Z-A', () => {
+  const arrDesc = [
+    { name: "bulbasaur" }, { name: "abra" }, { name: "charmander" }
+  ]
+  const resultadoDesc = [{ name: "charmander" }, { name: "bulbasaur" }, { name: "abra" }]
+  expect(ordenZaPokemon(arrDesc)).toEqual(resultadoDesc)
+});
+
+/*test 1-251*/
+
+describe("ordenAscendentePokemon", () => {
+  it("deberia ser una funcion", () => {
+
+
+    expect(typeof ordenAscendente).toBe("function");
+  });
+});
+it('Que retorne el array ordenado de 1-251', () => {
+  const arrAscent = [
+    { num: "1" }, { num: "66" }, { num: "12" }
+  ]
+  const resultadoAscent = [{ num: "1" }, { num: "12" }, { num: "66" }]
+  expect(ordenAscendente(arrAscent)).toEqual(resultadoAscent)
+});
+
+/* test 251-1*/
+describe("ordenDescendentePokemon", () => {
+  it("deberia ser una funcion", () => {
+
+
+    expect(typeof ordenDescendente).toBe("function");
+  });
+});
+it('Que retorne el array ordenado de 251-1', () => {
+  const arrDescent = [
+    { num: "1" }, { num: "66" }, { num: "12" }
+  ]
+  const resultadoDescent = [{ num: "66" }, { num: "12" }, { num: "1" }]
+  expect(ordenDescendente(arrDescent)).toEqual(resultadoDescent)
+});
+
+/* test filtro kanto*/
+describe("ordenKanto", () => {
+  it("deberia ser una funcion", () => {
+
+
+    expect(typeof ordenKanto).toBe("function");
+  });
+});
+it('Que retorne el array Kanto', () => {
+  const genKanto = {
+    generation: {
+      "name": "kanto"
+    },
+    generation2: {
+      "name": "johto"
+    },
+  }
+  const resultadoKanto = {
+    generation: {
+      "name": "kanto"
+    }
+  }
+
+  expect(ordenKanto(genKanto)).toEqual(resultadoKanto)
+});
+
+/* test filtro johto*/
+describe("ordenJohto", () => {
+  it("deberia ser una funcion", () => {
+
+
+    expect(typeof ordenJohto).toBe("function");
+  });
+});
+it('Que retorne el array Johto', () => {
+  const genJohto = {
+    generation: {
+      "name": "johto"
+    },
+    generation1: {
+      "name": "kanto"
+    },
+  }
+    const resultadoJohto = {
+      generation: {
+        "name": "johto"
+      }
+    }
+  
+  expect(ordenJohto(genJohto)).toEqual(resultadoJohto)
+});
